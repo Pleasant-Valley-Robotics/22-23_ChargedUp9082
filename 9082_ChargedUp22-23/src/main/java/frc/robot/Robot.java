cdcc4bc1f36f0ee23a0427ed5cdab70bd9df8d5c
BritneyBick <148918388+BritneyBick@users.noreply.github.com>
2023-12-21 19:02:32 -0600

    Bind xbox controller

--- 9082_ChargedUp22-23/src/main/java/frc/robot/Robot.java
+++ 9082_ChargedUp22-23/src/main/java/frc/robot/Robot.java
@@ -6,6 +6,7 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
@@ -26,7 +27,8 @@ public class Robot extends TimedRobot {
   private final MotorControllerGroup m_rightMotors = new MotorControllerGroup(m_rightMotor1, m_rightMotor2);
 
   private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftMotors, m_rightMotors);
-  private final Joystick m_stick = new Joystick(0);
+  //private final Joystick m_stick = new Joystick(0);
+  private final XboxController m_controller = new XboxController(0);
 
   @Override
   public void robotInit() {
@@ -41,6 +43,7 @@ public class Robot extends TimedRobot {
     // Drive with arcade drive.
     // That means that the Y axis drives forward
     // and backward, and the X turns left and right.
-    m_robotDrive.arcadeDrive(-m_stick.getY(), -m_stick.getX());
+    
+    m_robotDrive.arcadeDrive(-m_controller.getLeftY(), -m_controller.getLeftX());
   }
 }
